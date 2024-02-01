97e85e14258da602303ba975a21bd71dba4538e1
Patrick Ineichen <github@frokonet.ch>
2024-02-01 21:18:45 +0100

    chore: basic site setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import mdx from '@astrojs/mdx';
+import preact from '@astrojs/preact';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://frokonet.ch',
+  integrations: [tailwind(), preact(), mdx(), sitemap()]
+});
