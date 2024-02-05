083c6f288b5ab70ae426a0731fcb791bfbbdf32d
Patrick Ineichen <github@frokonet.ch>
2024-02-05 16:27:31 +0100

    feat: add Hero section

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,4 @@
 export * from './icons';
+
+export { default as Head } from './Head.astro';
+export { default as NavBar } from './NavBar.astro';
---
+++ src/data/nav.ts
@@ -0,0 +1,14 @@
+export const nav = [
+  {
+    title: 'Blog',
+    slug: '#'
+  },
+  {
+    title: 'About',
+    slug: '#'
+  },
+  {
+    title: 'Bookmarks',
+    slug: '#'
+  }
+];
