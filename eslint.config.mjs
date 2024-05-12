29d61dfd133c1b281f54edeb2817c548962f3e9f
Patrick Ineichen <github@frokonet.ch>
2024-05-12 21:31:37 +0200

    chore(deps): update dependencies (#67)

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintPluginAstro from 'eslint-plugin-astro';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
+  ...eslintPluginAstro.configs.recommended,
+  eslintConfigPrettier
+);
