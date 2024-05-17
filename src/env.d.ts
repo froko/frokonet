3865c992de5648326a28f2b8748c703846ce3df5
Patrick Ineichen <github@frokonet.ch>
2024-05-17 19:43:10 +0200

    feat: remove theme change (#75)

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,2 @@
-import '../.astro/types';
-import 'astro/client';
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="../.astro/types.d.ts" />
---
+++ tailwind.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,tsx}'],
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        'catppuccin-macchiato': {
+          primary: '#8aadf4', // blue
+          secondary: '#f5bde6', // pink
+          accent: '#8bd5ca', // teal
+          neutral: '#181926', // crust
+          'base-100': '#24273a', // base
+          info: '#7dc4e4', // sapphire
+          success: '#a6da95', // green
+          warning: '#eed49f', // yellow
+          error: '#ed8796' // red
+        }
+      }
+    ]
+  }
+};
