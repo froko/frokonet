259daf667029c1629fcca7306ef74bdd879c0c85
Patrick Ineichen <github@frokonet.ch>
2024-07-20 18:24:58 +0200

    chore: use eslint-plugin-simple-import-sort plugin (#97)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-import { defineConfig } from 'astro/config';
-
 import mdx from '@astrojs/mdx';
 import preact from '@astrojs/preact';
-import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,9 @@
 import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import eslintPluginAstro from 'eslint-plugin-astro';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginAstro from 'eslint-plugin-astro';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   eslint.configs.recommended,
@@ -17,5 +18,15 @@ export default tseslint.config(
         ...globals.node
       }
     }
+  },
+  {
+    plugins: { 'simple-import-sort': simpleImportSort },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error'
+    }
+  },
+  {
+    ignores: ['.astro', 'dist']
   }
 );
