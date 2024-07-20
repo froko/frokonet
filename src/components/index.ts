6ee406b7db62fdc7d468b22be94c543b78bf41c8
Patrick Ineichen <github@frokonet.ch>
2024-07-20 18:24:21 +0200

    chore: use eslint-plugin-simple-import-sort plugin

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,4 @@
-export * from './icons';
-
 export { default as ArrowLink } from './ArrowLink.astro';
 export { default as Head } from './Head.astro';
+export * from './icons';
 export { default as NavBar } from './NavBar.astro';
