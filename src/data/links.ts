555b7760f13cb5054bad872a63c64c827065fb35
Patrick Ineichen <github@frokonet.ch>
2024-11-10 20:01:01 +0100

    feat: add more bookmarks (#274)

--- src/data/links.ts
+++ src/data/links.ts
@@ -7,11 +7,69 @@ const links = [
     tags: ['Frontend', 'Angular'],
   },
   {
+    name: 'AnalogJs',
+    description: 'The fullstack Angular meta-framework',
+    url: 'https://analogjs.org/',
+    icon: 'https://analogjs.org/img/favicon.ico',
+    tags: ['Frontend', 'Angular', 'Meta Framework'],
+  },
+  {
+    name: 'NextJS',
+    description: 'The React Framework for the Web',
+    url: 'https://nextjs.org/',
+    icon: 'https://nextjs.org/favicon.ico',
+    tags: ['Frontend', 'React', 'Meta Framework'],
+  },
+  {
+    name: 'Nuxt',
+    description: 'The Intuitive Vue Framework',
+    url: 'https://nuxt.com/',
+    icon: 'https://nuxt.com/icon.png',
+    tags: ['Frontend', 'Vue', 'Meta Framework'],
+  },
+  {
+    name: 'Qwik',
+    description: 'Deliver instant apps at scale',
+    url: 'https://qwik.dev/',
+    icon: 'https://qwik.dev/favicons/favicon.svg',
+    tags: ['Frontend', 'Qwik'],
+  },
+  {
+    name: 'QwikCity',
+    description: 'Qwik Metaframework',
+    url: 'https://qwik.dev/docs/qwikcity',
+    icon: 'https://qwik.dev/favicons/favicon.svg',
+    tags: ['Frontend', 'Qwik', 'Meta Framework'],
+  },
+  {
     name: 'React',
-    description: 'A JavaScript library for building user interfaces',
+    description: 'The library for web and native user interfaces',
     url: 'https://react.dev',
     icon: 'https://react.dev/favicon-32x32.png',
     tags: ['Frontend', 'React'],
+  },
+  {
+    name: 'Remix',
+    description:
+      'Focused on web standards and modern web app UX, you’re simply going to build better websites',
+    url: 'https://remix.run/',
+    icon: 'https://remix.run/favicon-32.png',
+    tags: ['Frontend', 'React', 'Meta Framework'],
+  },
+  {
+    name: 'SolidJS',
+    description:
+      'Simple and performant reactivity for building user interfaces.',
+    url: 'https://www.solidjs.com',
+    icon: 'https://www.solidjs.com/img/favicons/favicon-32x32.png',
+    tags: ['Frontend', 'SolidJS'],
+  },
+  {
+    name: 'SolidStart',
+    description: 'Fine-grained reactivity goes fullstack',
+    url: 'https://start.solidjs.com/',
+    icon: 'https://start.solidjs.com/favicon.ico',
+    tags: ['Frontend', 'SolidJS', 'Meta Framework'],
   },
   {
     name: 'Vue',
@@ -21,12 +79,18 @@ const links = [
     tags: ['Frontend', 'Vue'],
   },
   {
-    name: 'Node.js',
-    description:
-      "Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine.",
-    url: 'https://nodejs.org',
-    icon: 'https://nodejs.org/static/images/favicons/favicon.png',
-    tags: ['Node', 'Runtime'],
+    name: 'Vite',
+    description: 'The Build Tool for the Web',
+    url: 'https://vite.dev/',
+    icon: 'https://vite.dev/logo.svg',
+    tags: ['Frontend', 'Vite', 'Build Tool'],
+  },
+  {
+    name: 'Vitest',
+    description: 'Next Generation Testing Framework',
+    url: 'https://vitest.dev',
+    icon: 'https://vitest.dev/logo.svg',
+    tags: ['Frontend', 'Testing', 'Quality'],
   },
 ];
 
